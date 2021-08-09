ebd73da398e5f85ecdf74e1378a7d4b8958d22ad
Behnamc <behnamrasooliamirabadi@gmail.com>
2021-08-09 13:03:30 +0430

    only_login

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('', views.blog_list, name="list"),
+    path('create', views.create_post, name="create"),
     path('<id>', views.post_detail, name="detail"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from . import models
+from django.contrib.auth.decorators import login_required
 
 def blog_list(request):
     posts = models.Post.objects.all().order_by('-date')
@@ -9,3 +10,7 @@ def blog_list(request):
 def post_detail(request, id):
     post = models.Post.objects.get(id=id)
     return render(request, 'blog/postDetail.html',{'post':post})
+
+@login_required(login_url='/accounts/login')
+def create_post(request):
+    return render(request, 'blog/create_post.html')
