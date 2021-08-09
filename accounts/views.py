55d05ddd8dcb62c545925a3cc1a69744c66f053a
Behnamc <behnamrasooliamirabadi@gmail.com>
2021-08-09 13:26:25 +0430

    end_logining & create page

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,10 @@ def login_view(request):
             # login user
             user = form.get_user()
             login(request, user)
-            return redirect('blog:list')
+            if 'next' in request.POST:
+                return redirect(request.POST.get("next"))
+            else:
+                return redirect('blog:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form':form})
